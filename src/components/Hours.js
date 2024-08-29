5c742c55ad58b82ede5109d76c23c9d5453410cc
Carlos Giles <92232878+CarlosGiles@users.noreply.github.com>
2024-08-29 18:25:32 +0000

    Añadido el componente Hours para mostrar los horarios de apertura

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,23 @@
 // Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+  const today = new Date();
+  const day = today.getDay(); // 0 (Domingo) - 6 (Sábado)
+  let hours;
+
+  if (day >= 1 && day <= 5) { // Lunes a Viernes
+    hours = '10 a.m. - 4 p.m.';
+  } else { // Sábado y Domingo
+    hours = '9 a.m. - 8 p.m.';
+  }
+
+  return (
+    <div>
+      <h2>Horario de hoy:</h2>
+      <p>{hours}</p>
+    </div>
+  );
+};
+
+export default Hours;
